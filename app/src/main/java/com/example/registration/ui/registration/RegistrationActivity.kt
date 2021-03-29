0a2a938b13d1cec0f11209ae5b2b11248286d2b6
TimurImankulov <timur.imankulov1984@gmail.com>
2021-03-29 21:48:55 +0600

    init commit

---
+++ app/src/main/java/com/example/registration/ui/registration/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.registration.ui.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.registration.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
